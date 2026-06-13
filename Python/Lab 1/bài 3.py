eab208471ca9959a248e8726164160b49e20d56a
hophii <phile2k2@gmail.com>
2026-06-13 14:49:16 +0700

    Xoa các file day nham

--- Python/Lab 1/bài 3.py
+++
@@ -1,7 +0,0 @@
-# Vòng lặp này sẽ chạy từ 1 đến 9
-for i in range(1, 10):
-  danh_sach = []
-  # Vòng lập này sẽ chạy từ 1 đến i, j là các số cần tính bình phương
-  for j in range(1, i + 1):
-    danh_sach.append(j**2) # append thêm j vào cuối danh sách
-  print(danh_sach)
--- shuffle_names.py
+++
@@ -1,8 +0,0 @@
-import random
-
-danh_sach = ["Tuyết", "Bảo", "Uyên", "Băng", "Nguyên", "Phii", "Tuấn", "Quàng"]
-random.shuffle(danh_sach)
-
-print("Thứ tự sau khi xáo trộn:")
-for i, ten in enumerate(danh_sach, 1):
-    print(f"{i}. {ten}")
